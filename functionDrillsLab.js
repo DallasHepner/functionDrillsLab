6daea733c439c6499ebd960275eed527773ff0dd
Dallas Hepner <dallashepner@gmail.com>
2022-06-21 17:50:06 -0600

    Third Commit

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -262,18 +262,26 @@ function bigOrSmall(arr){
   let arrayEvaluator = answers
   return arrayEvaluator;
 }
-console.log(bigOrSmall(bigOrSmallArray))
+// console.log(bigOrSmall(bigOrSmallArray))
 
 ////////////////// PROBLEM 16 ////////////////////
 let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
 let loser = 'Glimmer'
 /*
-  Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
-  The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
+  Write a function that is called theEliminator, which takes in two arguments, contestants 
+  (which will each be an array of strings), and loser (which will be a string).
+  The function should loop over the array of contestant names. 
+  If the loser string appears in the array, splice it out. Return the new contestants array.
 */
 
-//CODE HERE
-
+function theEliminator(a, b) {
+  for (let i = 0; i < contestants.length; i++) {
+    if (a[i] === b) {
+       contestants.splice(i, 1)
+    }
+  } return contestants
+}
+// console.log(theEliminator(contestants, loser))
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
@@ -282,8 +290,11 @@ let sampleString = "Hi, my name is Kylo."
   Invoke the function, passing in the sampleString (above).
 */
 
-//CODE HERE
-
+function capitolCase(letter){
+  const big = letter.toUpperCase();
+  console.log(big)
+}
+// capitolCase(`i want this big`)
 
 ////////////////// PROBLEM 18 ////////////////////
 /*
@@ -296,14 +307,32 @@ let sampleString = "Hi, my name is Kylo."
   return 'must provide a valid email address'
 */
 
+function emailCheck(email){
+  let mailStr = String(email);
+  let spaceStr = mailStr.trim(mailStr);
+  let letterCheck = spaceStr.includes('@');
+  if (letterCheck === true){
+    return `email verified`
+  }else{
+    return `must provide a valid email address`
+  }
+}
+// console.log(emailCheck(`joe.bob@yahoo.com`))
+
 ////////////////// PROBLEM 19 ////////////////////
 /*
-  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
+  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain 
+  amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, 
+  which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were 
+  able to purchase.
   Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
 */
 
-//CODE HERE
-
+function frogBuy(funds){
+  let totalFrogs = Math.round(funds/3);
+  return totalFrogs;
+}
+// console.log(frogBuy(7))
 
 ////////////////// PROBLEM 20 ////////////////////
 /*
@@ -319,7 +348,20 @@ let sampleArray = [0,1,2,3,4,7,5,6,8,9]
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
-//CODE HERE
+const isAscending = arr => {
+  let flag;
+  for (let i = 0; i < arr.length-1; i++){
+    if (arr[i] < arr[i+1] && arr[i+1] === arr[i] + 1){
+      flag = true;
+    } else{
+      flag = false;
+      break;
+    }
+  }
+  return flag;
+}
+let arrayIsAscending = isAscending(sampleArray);
+// console.log(arrayIsAscending);
 
 
 ////////////////// PROBLEM 22 ////////////////////
